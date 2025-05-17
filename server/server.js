4dd36956676876507c985be247e1e904d9c6fbef
Sohom Bose <143876774+sohombose@users.noreply.github.com>
2025-05-17 13:05:02 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,33 @@
+// server/server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('send_message', (data) => {
+    const messageData = { ...data, id: socket.id };
+    io.emit('receive_message', messageData); // send to all (including sender)
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
